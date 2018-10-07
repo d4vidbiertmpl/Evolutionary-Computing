3a81bb13300619b0a1f0636a5ad2dd327f0df300
David Biertimpel <david.biertimpel@protonmail.com>
2018-10-07 20:51:27 +0200

    Elitist clustering and proletarian group creation implemented

--- player31.java
+++ player31.java
@@ -22,7 +22,12 @@ public class player31 implements ContestSubmission {
     private int evaluations_limit_;   // number of allowed evaluations (defined by framework)
     private int evaluations_counter_ = 0; // counter for performed evaluations, starts at 0
 
-    private int population_size_ = 50; // size of one population
+    private int population_size_ = 100; // size of one population
+    private int elitist_size_ = 10;
+    private int proletarian_size_ = 20;
+
+    private double cluster_distance_thresh_ = 2.0;
+
 
     // PARAMETERS FOR PARENT SELECTION
     private int tournament_size_ = 5;
@@ -120,12 +125,26 @@ public class player31 implements ContestSubmission {
         System.out.println("--------------------------------");
     }
 
+    private void printClusters(double[][] clusters) {
+        for (int i = 0; i < clusters.length; i++) {
+            double[] cluster = clusters[i];
+            for (int j = 0; j < individual_size_; j++) {
+                String s = Double.toString(cluster[j]);
+                System.out.print(s.substring(0, 5).concat("\t"));
+            }
+            System.out.println("\n------------------------------------------------------------------------------\n");
+        }
+        System.out.println("\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
+    }
+
     private void printIndividual(Individual in) {
-        double values[] = in.getValues();
+        double[] values = in.getValues();
+        double fitness = in.getFitness();
         for (int i = 0; i < values.length; i++) {
             String s = Double.toString(values[i]);
             System.out.print(s.substring(0, 5).concat("\t"));
         }
+        System.out.println(fitness);
         System.out.println("\n------------------------------------------------------------------------------\n");
     }
 
@@ -232,12 +251,271 @@ public class player31 implements ContestSubmission {
         }
     }
 
+    private double[][] calcElitistCluster(ArrayList<Individual> elitist, double distance_threshold) {
+        /*
+         * Performs hierarchical clustering with the single linkage technique to determine the number of clusters in the
+         * elitist group
+         *
+         * @param elitist: The elitist group of the current generation
+         *
+         * @returns cluster_centroids: Returns the cluster of each found centroid
+         *
+         * */
+
+
+        // perform hierarchical clustering with single linkage
+        // check bottom up each hierarchy level and test if the distance threshold t is exceeded
+        // The first level where this is not violated determines the found clusters
+
+        double[][] distance_matrix = new double[elitist_size_][elitist_size_];
+
+        // fill distance matrix of elitist individuals
+        for (int i = 0; i < elitist_size_; i++) {
+            Individual current_elite = elitist.get(i);
+            for (int j = 0; j < elitist_size_; j++) {
+                Individual other_elitist = elitist.get(j);
+                if (i == j) {
+                    distance_matrix[i][j] = Double.MAX_VALUE;
+                } else {
+                    double distance = euclideanDistance(current_elite.getValues(), other_elitist.getValues());
+                    distance_matrix[i][j] = distance;
+                }
+            }
+        }
+
+        // print distance matrix for testing
+//        for (int i = 0; i < elitist_size_; i++) {
+//            double[] distance_ind_i = distance_matrix[i];
+//            for (int j = 0; j < elitist_size_; j++) {
+//                System.out.print(distance_ind_i[j]);
+//                System.out.print(" ++ ");
+//            }
+//            System.out.println("\n------------------------------------------------------------------------------\n");
+//        }
+
+        Map<Integer, Integer> hierarchical_clusters = new HashMap<Integer, Integer>();
+
+        // initialize clustering => each individual is its own cluster
+        for (int i = 0; i < elitist_size_; i++) {
+            hierarchical_clusters.put(i, i);
+        }
+
+        double current_distance = 0.0;
+
+        while (distance_threshold > current_distance) {
+            int[] indices = findMin(distance_matrix);
+            int i = indices[0];
+            int j = indices[1];
+
+            // min value => current_distance
+            current_distance = distance_matrix[i][j];
+
+            if (distance_threshold > current_distance) {
+                int corres_cluster = hierarchical_clusters.get(i);
+                hierarchical_clusters.put(j, corres_cluster);
+
+                distance_matrix[i][j] = Double.MAX_VALUE;
+                distance_matrix[j][i] = Double.MAX_VALUE;
+            }
+        }
+
+//        for (int i = 0; i < hierarchical_clusters.size(); i++) {
+//            int cluster_no = hierarchical_clusters.get(i);
+//            System.out.print(i);
+//            System.out.print(" MAPPES TO CLUSTER ");
+//            System.out.print(cluster_no);
+//            System.out.println("-----------------------------------");
+//        }
+
+        // convert integer cluster representation to individuals, to calculate centroid and radius
+        Map<Integer, ArrayList<Individual>> cluster_to_ind = new HashMap<Integer, ArrayList<Individual>>();
+        int current_cluster_count = 0;
+        for (int i = 0; i < elitist_size_; i++) {
+            int no_current_cluster = hierarchical_clusters.get(i);
+            if (cluster_to_ind.containsKey(no_current_cluster)) {
+                cluster_to_ind.get(no_current_cluster).add(elitist.get(i));
+            } else {
+                ArrayList<Individual> new_cluster = new ArrayList<Individual>();
+                new_cluster.add(elitist.get(i));
+                cluster_to_ind.put(current_cluster_count, new_cluster);
+                current_cluster_count++;
+            }
+        }
+
+//        for (int i = 0; i < cluster_to_ind.size(); i++) {
+//            if (cluster_to_ind.containsKey(i)) {
+//                ArrayList<Individual> ind_list = cluster_to_ind.get(i);
+//                System.out.println(i);
+//                System.out.println(ind_list.size());
+//                for (int j = 0; j < ind_list.size(); j++) {
+//                    printIndividual(ind_list.get(j));
+//                }
+//                System.out.println("\n+++++++++++++++++++++++++++++++++++++++++++++++++\n");
+//            }
+//        }
+
+        // Calculate centroid of each cluster
+        double[][] cluster_centroids = new double[cluster_to_ind.size()][individual_size_];
+        for (int i = 0; i < cluster_to_ind.size(); i++) {
+            ArrayList<Individual> cluster = cluster_to_ind.get(i);
+            double[] centroid = calcCentroid(cluster);
+            cluster_centroids[i] = centroid;
+        }
+
+        return cluster_centroids;
+
+    }
+
+    /*
+     * Calculates centroid of one cluster
+     *
+     * @param: Individuals in an ArrayList representing a cluster.
+     * */
+    private double[] calcCentroid(ArrayList<Individual> cluster) {
+        double[] sum_centroid = new double[individual_size_];
+
+        for (int i = 0; i < cluster.size(); i++) {
+            double[] ind_vec = cluster.get(i).getValues();
+
+            for (int j = 0; j < individual_size_; j++) {
+                double dim_value = ind_vec[j];
+                sum_centroid[j] += dim_value;
+            }
+        }
+
+        double[] centroid = new double[individual_size_];
+        for (int i = 0; i < individual_size_; i++) {
+            centroid[i] = sum_centroid[i] / cluster.size();
+        }
+
+        return centroid;
+    }
+
+
+    private int[] findMin(double[][] distance_matrix) {
+        /*
+         * Finds the minimum value in a two dimensional double array.
+         *
+         * @param distance_matrix: distance matrix representing the euclidean distance between the individuals.
+         *
+         * @return double[][]: the coordinates (i, j) of the minimum in the table.
+         * */
+        int min_i = 0;
+        int min_j = 0;
+
+        double min_value = Double.MAX_VALUE;
+
+        for (int i = 0; i < elitist_size_; i++) {
+            for (int j = 0; j < elitist_size_; j++) {
+                double current_value = distance_matrix[i][j];
+                if (current_value < min_value) {
+                    min_value = current_value;
+                    min_i = i;
+                    min_j = j;
+                }
+            }
+
+        }
+
+        return new int[]{min_i, min_j};
+    }
+
+
+    private double euclideanDistance(double[] a, double[] b) {
+        /*
+         * Calculates the euclidean distance between two individuals
+         *
+         * @param a, b: the position of an individual in the search space
+         *
+         * @return: The euclidean distance
+         * */
+        double sum = 0;
+        for (int i = 0; i < individual_size_; i++) {
+            sum = sum + Math.pow((a[i] - b[i]), 2.0);
+        }
+        return Math.sqrt(sum);
+    }
+
+    private ArrayList<Individual> getElitistGroup(ArrayList<Individual> population) {
+        /*
+         * Finds the elitist group of the population.
+         *
+         * @param population: the current population
+         *
+         * @return elitist: the elitist group
+         *
+         * */
+        ArrayList<Individual> elitist = new ArrayList<Individual>();
+        Collections.sort(population);
+
+        for (int i = 0; i < elitist_size_; i++) {
+            elitist.add(population.get(i));
+        }
+
+        return elitist;
+    }
+
+
+    private Individual createRandomIndividual() {
+        /*
+         * Creates a random individual in the search space and returns it.
+         * */
+
+        double[] individual = new double[individual_size_];
+        for (int i = 0; i < individual_size_; i++) {
+            double random_double = values_min_ + (values_max_ - values_min_) * rnd_.nextDouble();
+            individual[i] = random_double;
+        }
+        //evaluate generated individual
+        double fitness_i = (double) evaluation_.evaluate(individual);
+        evaluations_counter_ += 1;
+
+        return new Individual(individual, fitness_i);
+    }
+
+    private boolean isIndividualInCluster(double[][] clusters, Individual individual) {
+        /*
+         * Tests if an individual is in the spatial range of a cluster.
+         * */
+
+        for (int i = 0; i < clusters.length; i++) {
+            double[] current_cluster = clusters[i];
+            double distance = euclideanDistance(current_cluster, individual.getValues());
+
+            if (distance < cluster_distance_thresh_) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private ArrayList<Individual> generateRandomProletarians(ArrayList<Individual> population, double[][] cluster) {
+        /*
+        * Deletes the worst individuals in a population depending on the proletarian_size_ and refills the population
+        * with random individuals outside the clusters determined by the individuals of the elitist group.
+        * */
+
+        Collections.sort(population);
+        population = new ArrayList<Individual>(population.subList(0, (population_size_ - proletarian_size_)));
+
+        int current_proletarians = 0;
+        while (current_proletarians < proletarian_size_) {
+            Individual proletarian = createRandomIndividual();
+            if (!isIndividualInCluster(cluster, proletarian)) {
+                population.add(proletarian);
+                current_proletarians++;
+            }
+
+        }
+        return population;
+    }
+
+
 /*
     TODO: Implement first basic approach => maybe at least non-uniform mutation for that?
     TODO: Improve parent selection? => what selection pressure do we want to have?
     TODO: Implement more sophisticated approach with uncorrelated mutation with n step size
-    TODO: Extend both approaches with elitist / proletarian group
-    TODO: Implement hierarchical clustering on individuals && create new individuals outside clusters
+    TODO: Extend both approaches (simple and sophisticated) with the proletarian group approach
     TODO: Implement function to save data to csv files for visualization
     TODO: Implement visualization => reason about data ...
 */
@@ -251,9 +529,18 @@ public class player31 implements ContestSubmission {
         // Rank population in fitness
         Collections.sort(population);
 
-        //printPopulationFitness(population); // just for testing
+        // print initial population for testing
+//        for (int i = 0; i < population_size_; i++) {
+//            printIndividual(population.get(i));
+//        }
+//        printPopulationFitness(population); // just for testing
 
         while (evaluations_counter_ < evaluations_limit_) {
+
+            ArrayList<Individual> elitist = getElitistGroup(population);
+            double[][] elitist_clusters = calcElitistCluster(elitist, cluster_distance_thresh_);
+//            printClusters(elitist_clusters);
+            System.out.println(elitist_clusters.length);
 
 
             // Create next generation (offspring)
@@ -283,7 +570,6 @@ public class player31 implements ContestSubmission {
                 offspring.addAll(children);
             }
 
-
             // SURVIVOR SELECTION
             // (1) Just replace whole population by offspring
             //  population = new ArrayList<Individual>(offspring);
@@ -293,6 +579,8 @@ public class player31 implements ContestSubmission {
             Collections.sort(population);
             population = new ArrayList<Individual>(population.subList(0, population_size_));
 
+// Function that introduces the random individuals => just needs to be properly embedded into the evaluations (causes Null pointer at the moment)
+//            population = generateRandomProletarians(population, elitist_clusters);
 
         }
     }
@@ -304,7 +592,7 @@ class Individual implements Comparable<Individual> {
     Class that wraps a single individual and its fitness.
     Enables easy comparison among individuals.
     */
-    private double values_[];
+    private double[] values_;
     private double fitness_;
 
     public Individual(double values[]) {
