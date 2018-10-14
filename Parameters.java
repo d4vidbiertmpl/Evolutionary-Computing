37ff2ea72a4c7347a80f75d4937f57897c372035
Bobbie <bobbievangorp@live.nl>
2018-10-14 18:09:16 +0200

    Change parameters to optimal values for Schaffers, add tuning_results

--- Parameters.java
+++ Parameters.java
@@ -13,16 +13,16 @@ class Parameters{
 
 
     // Parameters for the clustering
-    public int elitist_size = 10;
-    public int proletarian_size = 20;
-    public double cluster_distance_thresh = 2.0;
+    public int elitist_size = 5;
+    public int proletarian_size = 13;
+    public double cluster_distance_thresh = 1.3007402405183437;
 
 
     // Parameters for the EA-components
-    public int offspring_size = 150;  //TODO: Should that be more/less ?
+    public int offspring_size = 100;  //TODO: Should that be more/less ?
 
     // (1) Parameters for parent selection
-    public int parent_tournament_size = 5;
+    public int parent_tournament_size = 14;
 
     // (2) Parameters for mutation
     public double uniform_mutation_prop = 0.05;
@@ -32,13 +32,13 @@ class Parameters{
     public double tau = 1 / Math.sqrt(2*individual_size);
     public double tau_prime = 1 / Math.sqrt(2*Math.sqrt(individual_size));
     public double boundary = 0.01;
-
+    
     // (3) Parameters for survivor selection
-    public int survivor_tournament_size = 10;
+    public int survivor_tournament_size = 4;
 
     // (4) Parameters for hybridisation
-    public int evaluations_per_proletarian = 50;
+    public int evaluations_per_proletarian = 81;
     public boolean use_hybridisation = true; //If true, hybridisation will be used in the 'sophisticated approach with own' on multimodal functions only
-    public double hill_climb_step_size = 1.0;
+    public double hill_climb_step_size = 0.6827029110776486;
 
 }
